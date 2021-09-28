fbf4c4ba3da4c3c156f914b6d3a12a712d80269e
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-09-28 11:28:11 +0200

    user dto added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('user')
-  async createUser(): Promise<any> {
-    return await this.userService.createUser();
+  async createUser(@Body() createUserData: CreateUserDto): Promise<any> {
+    return await this.userService.createUser(createUserData);
   }
 }
