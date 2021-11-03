5f3b60cec27f07b47a766c82b2c6fd3deed4faeb
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2021-11-03 14:30:02 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get('PORT') || 3000);
 }
 
 bootstrap();
